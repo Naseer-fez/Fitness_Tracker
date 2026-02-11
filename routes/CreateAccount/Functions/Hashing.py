322ce58fded0c4022d910aeaf2a759f33ec8d219
Naseer <feznaseer777@gmail.com>
2026-02-11 14:42:32 +0530

    Hashing Implemantaion

--- routes/CreateAccount/Functions/Hashing.py
+++ routes/CreateAccount/Functions/Hashing.py
@@ -1,2 +1,18 @@
+import ctypes as C
+import os 
+curentpath=os.path.dirname(os.path.abspath(__file__))
+
 def hashdata(username,password):
+    hash=C.CDLL(os.path.join(curentpath,"Hashfun.dll"))
+    hashfun=hash.Hashreturn
+    hashfun.argtypes=[C.c_char_p,C.c_char_p]
+    hashfun.restype=C.c_char_p
+    encuser=username.encode('utf-8')
+    encpas=password.encode('utf-8')
+    password=hashfun(encuser,encpas).decode('utf-8')
+
     return [username,password]
+
+
+if __name__=="__main__":
+    print(hashdata("A",'A')[1])
--- routes/Login/Functions/ReHashing.py
+++ routes/Login/Functions/ReHashing.py
@@ -1,2 +1,25 @@
-def rehashdata(username,password):
-    return [username,password]
+import ctypes as C
+import os 
+curentpath=os.path.dirname(os.path.abspath(__file__))
+
+
+
+
+def rehashdata(username,password,hashedpassword):
+    hash=C.CDLL(os.path.join(curentpath,"Rehash.dll"))
+    reshah=hash.Rehash
+    reshah.argtypes=[C.c_char_p,C.c_char_p,C.c_char_p]
+    reshah.restype=C.c_int
+    encuser=username.encode()
+    encpas=password.encode()
+    if isinstance(hashedpassword, bytes):
+        enchash = hashedpassword
+    else:
+        enchash = hashedpassword.encode('utf-8')
+    checking=reshah(encuser,encpas,enchash)
+    if  checking==1:
+        return 1
+    else:
+        return 0
+
+    
