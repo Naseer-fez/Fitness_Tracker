c5f69cde6121779cd43b95c004d507c32d0c857d
Naseer <feznaseer777@gmail.com>
2026-02-26 22:48:11 +0530

    Succes in Calander implemtation

--- models/Calander_Table.py
+++ models/Calander_Table.py
@@ -1,9 +1,9 @@
-from flask_sqlalchemy import SQLAlchemy
 from models.Sql_Tables import db
-
+from sqlalchemy.dialects.mysql import TINYINT
 
 class Calander(db.Model):
     __tablename__="Calander"
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True, nullable=False)
     dates=db.Column(db.String(500) ,nullable=False)
+    month = db.Column(TINYINT(unsigned=True), nullable=False)
     
--- routes/Workout/Calander.py
+++ routes/Workout/Calander.py
@@ -21,12 +21,13 @@ current_month_int = now.month
 current_year_int = now.year   
 weeks = calendar.monthcalendar(current_year_int, current_month_int)
 
-def transfer_to_db(userid,info):
+def transfer_to_db(userid,info,des_mon=current_month_int):
     if(isinstance(info,list)):
         output="".join(info)
     else:
         output=info
-    trans=cal(user_id=userid,dates=output)
+    
+    trans=cal(user_id=userid,dates=output,month=des_mon)
     try:
         # db.session.add(trans)
         db.session.merge(trans)
@@ -137,23 +138,36 @@ def data(user_name,update=0):
     found_Cal=cal.query.filter_by(user_id=id).first()
     if found_Cal is None:
         info=dates_maker(week=weeks,todaysdate=date)
-        transfer_to_db(userid=id,info=info)
+        transfer_to_db(userid=id,info=info,des_mon=current_month_int)
         return info #the data is added into the db now , need to think about what if the data is nto added
     info=found_Cal.dates
     today_idx = get_today_index(todaysdate=date)
 
     elemnt=info[today_idx]
-    print(elemnt)
+    # print(elemnt)
+    table_month=found_Cal.month
+    send_mon=current_month_int
+    flag=0
+    if(table_month!=send_mon):
+        info=dates_maker(week=weeks,todaysdate=date)
+        transfer_to_db(userid=id,info=info,des_mon=current_month_int)
+        flag=1
+        elemnt = info[today_idx]
+    else:
+        info=found_Cal.dates
+
+       
+            
     if update==0:
         output= date_updates(week=info,todaysdate=today_idx,status=elemnt)
+        if flag==1:
+            transfer_to_db(userid=id,info=output,des_mon=send_mon)
+
         return output
     else:
-        
-        # if info[today_idx] == Succues:
-        #     return info
         output= date_updates(week=info,todaysdate=today_idx,status=Succues)
-        transfer_to_db(userid=id,info=output)
-                # print(output)
+        transfer_to_db(userid=id,info=output,des_mon=send_mon)
+
         return output
     
 
