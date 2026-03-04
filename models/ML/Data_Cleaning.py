06a17d15580a3a0c1cf6e32b9425a0761387e873
Naseer <feznaseer777@gmail.com>
2026-03-04 15:04:47 +0530

    Data Cleaned

--- models/ML/Data_Cleaning.py
+++ models/ML/Data_Cleaning.py
@@ -1,40 +1,73 @@
 import pandas as pd
-import yaml
+import pickle
+from pathlib import Path
 data=pd.read_csv(r"Data\gym_members_exercise_tracking.csv")
-
-def save_schema(cols_list, filename="schema.yaml"):
-    schema = {
-       "features": {f"set_{i}": list(cols) for i, cols in enumerate(cols_list)}
-    }
-    
-    with open(filename, "w") as file:
-        yaml.dump(schema, file)
+pd.set_option('display.max_rows', 100)
 
 
-# print(data.info())
-d={"Male":"male","Female":"female"}
-data['Gender'] = data['Gender'].map(d)
+def loging(info,mode='w'):
+    with open("Data/Test.txt",f'{mode}') as f:
+        f.write(f"{info}\n")
+    print("Done")
+
+def Outputing_rows(filename,filemode,output):
+    filepath=Path("Data/Rows")
+    if filepath.exists():
+        pass
+    else:
+        filepath.mkdir(parents=True, exist_ok=True) 
+    with open(f"{filepath}/{filename}.pkl",mode=filemode) as f:
+        pickle.dump(output,f)
 
 
-# print(data.info())
-# 
-data.columns = [c.replace('Workout_Frequency (days/week)', 'Daysofweek') for c in data.columns]
-# data["Daysofweek"]=data["Workout_Frequency (days/week)"]
-data=data.drop(columns=["Experience_Level","Water_Intake (liters)","Calories_Burned",
-                        "Workout_Type","Session_Duration (hours)","Gender"],axis=1)
 
-# print(data.info())
-# print(data["Daysofweek"])
-heartdetails = data[["Max_BPM","Avg_BPM","Resting_BPM","Age","Fat_Percentage"]].copy()
-# print(heartdetails)
+rename_map = {
+    "Age": "Age",
+    "Gender": "gender",
+    "Weight (kg)": "weight",
+    "Height (m)": "height",
+    "Session_Duration (hours)": "Protien_Difference",
+    "Workout_Frequency (days/week)": "Daysofweek",
+    "BMI": "BMI"
+}
+data.rename(columns=rename_map, inplace=True)
 
-heartdetails['HR_Reserve'] = heartdetails['Max_BPM'] -heartdetails['Resting_BPM']
-heartdetails['BPM_Range'] = heartdetails['Max_BPM'] - heartdetails['Avg_BPM']
-heartdetails['Intensity_Score'] = (heartdetails['Avg_BPM'] / heartdetails['Max_BPM']) * 100
+SimilarColoums = [
+    "Age", 
+    "gender", 
+    "weight", 
+    "height", 
+    "Protien_Difference", 
+    "Daysofweek", 
+    "BMI"
+]
 
-heartdetails.to_csv(r"Data\Heart_details.csv", index=False)
-data.to_csv(r"Data\ML_Details.csv")
+Heartrealted=[ 
+"Max_BPM",
+"Avg_BPM",
+"Resting_BPM"]
+drop=[
+"Calories_Burned",
+"Workout_Type",
+"Water_Intake (liters)",
+"Experience_Level"]
+important=[
+    "Fat_Percentage",
+    "Experience_Level"
+]
 
-save_schema(cols_list=[heartdetails.columns,data.columns],filename=r"DATA\DATA.yaml")
+# Similar=data.drop(columns=((Heartrealted)),inplace=False)
+Similardata=data[SimilarColoums]
+Heartdata=data[Heartrealted]
+Extradata=data[drop]
+Ml_Data=data[important]
 
+alltherows=["Similardata","Heartdata","Extradata","Ml_Data"]
 
+Final_Data = {name: locals()[name] for name in alltherows}
+# for i in Final_Data:
+#     loging(i.columns.tolist(),'a')
+# print(Final_Data)
+Outputing_rows(filename="Allrows",filemode='wb',output=alltherows)
+for row in list(Final_Data.keys()):
+    Outputing_rows(filename=row,filemode='wb',output=Final_Data[row])
--- models/ML/ML_Implemtion.py
+++ models/ML/ML_Implemtion.py
@@ -1,47 +1,31 @@
-import yaml
-from sklearn.linear_model import Ridge
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-import pandas as pd
-
-def load_schema(filename=r"DATA\DATA.yaml"):
-    with open(filename, "r") as file:
-        return yaml.safe_load(file)
-
-def save_to_yaml(data, filename=r"DATA\Final_Data.yaml"):
-    with open(filename, "w") as file:
-        yaml.dump(data, file, default_flow_style=False)
-
-schema = load_schema()
-
-Heartdetails = schema["features"]["set_0"]
-Data = schema["features"]["set_1"]
-Fetures=Heartdetails+Data
-heartdata=pd.read_csv(r"Data/Heart_details.csv")
-maindata=pd.read_csv(r"Data/ML_Details.csv")
-df=pd.concat([heartdata, maindata], axis=1)
-target="Intensity_Score"
-
-X=df[Fetures]
-Y=df[target]
+import pickle
+from pathlib import Path
+import os 
+filepath=Path("Data/Rows")
+def Fileoperation(filename,filemode='rb',destination=filepath,info=None):
+    if filemode=='rb':
+        with open(f"{destination}/{filename}.pkl",mode=filemode) as f:
+            output=pickle.load(f)
+            return output
+    else:
+         with open(f"{destination}/{filename}.pkl",mode=filemode) as f:
+            pickle.dump(info,f)
 
 
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+def loging(info,mode='w'):
+    with open("Data/Test.txt",f'{mode}') as f:
+        f.write(f"{info}\n")
+    print("Done")
 
-model = Ridge(alpha=1.0)
-model.fit(X_scaled, Y)
-
-# Exporting parameters
-slopes = dict(zip(Fetures, model.coef_.tolist()))
-intercept = float(model.intercept_)
-
-schema['linear_model'] = {
-    'slopes': slopes,
-    'intercept': intercept,
-    'scaling_mean': dict(zip(Fetures, scaler.mean_.tolist())),
-    'scaling_var': dict(zip(Fetures, scaler.var_.tolist()))
-}
-
-save_to_yaml(schema)
-print("Data merged and coefficients saved to YAML.")
+# def findingfiles():
+#     files=[]
+#     for file in filepath.glob("*"):
+#     #  print(f"Found file: {file.name}")
+#         files.append(file.name)
+#     print(files)
+# print(Allrows)
+# Allrows=Fileoperation(filename="Allrows")
+Extradata=Fileoperation(filename="Extradata")
+Heartdata=Fileoperation(filename="Heartdata")
+Ml_data=Fileoperation(filename="Ml_data")
+Similardata=Fileoperation(filename="Similardata")
